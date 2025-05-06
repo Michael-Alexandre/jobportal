624ff1a2e08db1a41e1d8fde15eb7e56dd9a9cd7
Michael <michael.alexandre27@gmail.com>
2025-05-06 10:38:53 -0300

    Add user and user type entities with validation and relationships

---
+++ src/main/java/com/mike/jobportal/entity/Users.java
@@ -0,0 +1,43 @@
+package com.mike.jobportal.entity;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int userId;
+
+    @NotEmpty(message = "Pass cannot be empty")
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    private boolean isActive;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date registrationDate;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "userTypeId", referencedColumnName = "userTypeId")
+    private UsersType userTypeId;
+
+    
+}
---
+++ src/main/java/com/mike/jobportal/entity/UsersType.java
@@ -0,0 +1,26 @@
+package com.mike.jobportal.entity;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users_type")
+public class UsersType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int UserTypeid;
+    private String UserTypeName;
+
+    @OneToMany(targetEntity = Users.class, mappedBy = "userTypeId", cascade = CascadeType.ALL)
+    private List<Users> users;
+
+}
