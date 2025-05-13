140a389c7cc94b7d3cf6b4c49f95c5c4e3d1ec2f
Michael <michael.alexandre27@gmail.com>
2025-05-13 20:46:25 -0300

    Add user registration endpoint and dashboard template; update dependencies

--- src/main/java/com/mike/jobportal/controller/UsersController.java
+++ src/main/java/com/mike/jobportal/controller/UsersController.java
@@ -8,7 +8,13 @@ import org.springframework.ui.Model;
 import com.mike.jobportal.entity.Users;
 import com.mike.jobportal.entity.UsersType;
 import com.mike.jobportal.services.UsersTypeService;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
@@ -27,5 +33,11 @@ public class UsersController {
         model.addAttribute("getAllTypes", usersTypes);
         model.addAttribute("users", new Users());
         return "register";
-    }   
+    }
+
+    @PostMapping("/register/new")
+    public String userRegistration(@Valid Users users) {
+        System.out.println("User:."+users);
+        return "dashboard";
+    }
 }
